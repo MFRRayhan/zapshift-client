f2e1cde8d5cc0c4d79f8592078dcf3bc8c97e6d7
MFRRayhan <md.fazlerabbirayhan786@gmail.com>
2026-05-27 14:15:55 +0600

    add useRole custom hook for role-based access control

---
+++ src/hooks/useRole.jsx
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
+
+export default function useRole() {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { isLoading, data: role = user } = useQuery({
+    queryKey: ["user-role", user?.email],
+    enabled: !!user?.email,
+    queryFn: async () => {
+      const res = await axiosSecure(`/users/${user?.email}/role`);
+      return res.data;
+    },
+  });
+
+  return { isLoading, role };
+}
